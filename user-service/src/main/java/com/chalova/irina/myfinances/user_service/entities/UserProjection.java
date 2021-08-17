927eb0609bf2b963eb1f889438f0de4982206dac
Irina Chalova <chalovaira@gmail.com>
2021-08-17 12:21:05 +0300

    Delete unnecessary files

--- user-service/src/main/java/com/chalova/irina/myfinances/user_service/entities/UserProjection.java
+++
@@ -1,8 +0,0 @@
-package com.chalova.irina.myfinances.user_service.entities;
-
-import org.springframework.data.rest.core.config.Projection;
-
-@Projection(name = "productProjection", types = UserEntity.class)
-public interface UserProjection {
-
-}
