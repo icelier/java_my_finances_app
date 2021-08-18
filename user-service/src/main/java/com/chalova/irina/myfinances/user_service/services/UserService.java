2c8aba17736bfe26bb3f4185cb0c4111da0873f9
Irina Chalova <chalovaira@gmail.com>
2021-08-18 17:12:11 +0300

    Delete unnecessary files

--- user-service/src/main/java/com/chalova/irina/myfinances/user_service/services/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.chalova.irina.myfinances.user_service.services;
-
-import com.chalova.irina.myfinances.user_service.entities.UserEntity;
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface UserService extends UserDetailsService {
-    UserEntity findByUserName(String username);
-    boolean save(UserEntity user);
-}
